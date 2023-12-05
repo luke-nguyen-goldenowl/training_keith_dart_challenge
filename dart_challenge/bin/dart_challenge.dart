f04442b813421ae73fda3acb3583af727c55db86
Keith-Go <keith.vo.goldenowl@gmail.com>
2023-12-05 12:01:01 +0700

    Do unit test 1 dart challenge

--- dart_challenge/bin/dart_challenge.dart
+++ dart_challenge/bin/dart_challenge.dart
@@ -1,5 +1,7 @@
 import 'package:dart_challenge/exercise_1.dart' as dart_challenge;
+import 'package:dart_challenge/unit_test_1.dart' as unit_test_1;
 
 void main(List<String> arguments) {
   dart_challenge.exercise1();
+  print(unit_test_1.unitTest1(31));
 }
--- dart_challenge/test/dart_challenge_test.dart
+++ dart_challenge/test/dart_challenge_test.dart
@@ -1,3 +1,22 @@
+import 'package:dart_challenge/unit_test_1.dart' as unit_test_1;
+
+import 'package:collection/collection.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group("unit test 1", () {
+    test("input 20", () {
+      List<int> expected = [1, 2, 4, 5, 10, 20];
+      List<int> actual = unit_test_1.unitTest1(20);
+      final bool equal = ListEquality().equals(actual, expected);
+      expect(equal, true);
+    });
+
+    test("input 31", () {
+      List<int> expected = [1, 31];
+      List<int> actual = unit_test_1.unitTest1(31);
+      final bool equal = ListEquality().equals(actual, expected);
+      expect(equal, true);
+    });
+  });
+}
