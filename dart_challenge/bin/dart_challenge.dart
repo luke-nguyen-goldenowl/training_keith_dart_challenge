43e98d498e9b3d264d5aac0166d4ddc21f8a4212
Keith-Go <152830147+Keith-Go@users.noreply.github.com>
2023-12-05 14:24:52 +0700

    Update dart_challenge.dart

--- dart_challenge/bin/dart_challenge.dart
+++ dart_challenge/bin/dart_challenge.dart
@@ -31,7 +31,7 @@ void main(List<String> arguments) {
       case 1:
         dart_challenge.exercise1();
         break;
-      case 2:
+      case 3:
          print(unit_test_2.unitTest2([1, 1, 2, 3, 5, 8, 89, 13, 21, 34, 55],
       [1, 2, 3, 4, 5, 6, 10, 7, 8, 9, 11, 12, 13]));
         break;
