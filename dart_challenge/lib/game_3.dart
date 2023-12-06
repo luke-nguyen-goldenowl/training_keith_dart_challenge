e455508e5f30750baecc963833fd26b06629a8bb
Keith-Go <keith.vo.goldenowl@gmail.com>
2023-12-06 13:39:54 +0700

    improve game 3

--- dart_challenge/lib/game_3.dart
+++ dart_challenge/lib/game_3.dart
@@ -2,12 +2,10 @@ import 'dart:io';
 import 'dart:math';
 
 void game3() {
-  int cows = 0, bulls = 0, attempts = 0, oldCows = 0;
+  int cows = 0, bulls = 0, attempts = 0;
   Random random = new Random();
-  List<String> listNumber = [(random.nextInt(9) + 1).toString()];
-  for (var i = 0; i < 3; i++) {
-    listNumber.add(random.nextInt(10).toString());
-  }
+  List<String> listNumber =
+      (random.nextInt(9999 - 1000) + 1000).toString().split("");
 
   String? yourChoose = "";
 
@@ -24,8 +22,6 @@ void game3() {
       continue;
     }
 
-    oldCows = cows;
-
     attempts++;
     for (int i = 0; i < 4; i++) {
       if (listNumber.contains(yourChoose[i])) {
@@ -38,9 +34,11 @@ void game3() {
     }
     print("Attempts: $attempts\nCows: $cows, Bulls: $bulls");
 
-    if (cows - oldCows == 4) {
+    if (cows == 4) {
       print("Bingo!!!");
       break;
     }
+    cows = 0;
+    bulls = 0;
   } while (yourChoose != "exit");
 }
