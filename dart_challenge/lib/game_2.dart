428970dfb8d7fb65f3a6497dfdac46f108fc1efd
Keith-Go <keith.vo.goldenowl@gmail.com>
2023-12-06 09:53:44 +0700

    improve game 2

--- dart_challenge/lib/game_2.dart
+++ dart_challenge/lib/game_2.dart
@@ -12,7 +12,7 @@ void game2() {
         "\nType exit to quit the game\nPlease choose a number between 0 and 100:");
     select = stdin.readLineSync() ?? "exit";
 
-    if (select == "exit") break;
+    if (select == "exit") continue;
 
     if (int.parse(select) == number) {
       print("Bingo! You tried $count times\n");
