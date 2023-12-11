3a820476eef71dc6e33a576c2535902d985bfef3
Keith-Go <keith.vo.goldenowl@gmail.com>
2023-12-11 10:29:37 +0700

    add routeName class

---
+++ gallery_app/lib/configs/routes/routes_name.dart
@@ -0,0 +1,14 @@
+class RoutesName {
+  static const String home = "/";
+  static const String button = "/button";
+  static const String container = "/container";
+  static const String cupertino = "/cupertino";
+  static const String customBoxShape = "/custom_box_shape";
+  static const String imageTitleAppBar = "/image_title_app_bar";
+  static const String rowColumnExpanded = "/row_column_expanded";
+  static const String scaffold = "/scaffold";
+  static const String stackAlign = "/stack_align";
+  static const String textField = "/text_field";
+  static const String wrapChip = "/wrap_chip";
+  static const String textRichTextSpan = "/text_richtext_span";
+}
