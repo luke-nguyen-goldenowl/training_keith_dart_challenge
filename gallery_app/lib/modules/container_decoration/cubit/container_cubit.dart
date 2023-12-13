15c79a23e2ed8ec7b9d31fc5a9a62c31a81330bd
Keith-Go <keith.vo.goldenowl@gmail.com>
2023-12-13 16:27:28 +0700

    Add container screen

---
+++ gallery_app/lib/modules/container_decoration/cubit/container_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:gallery_app/modules/container_decoration/model/container_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ContainerCubit extends Cubit<ContainerState> {
+  ContainerCubit() : super(ContainerState());
+
+  void setBorder(bool border) {
+    emit(state.copyWith(border: border));
+  }
+
+  void setBorderRadius(dynamic radius) {
+    emit(state.copyWith(borderRadius: radius));
+  }
+
+  void setBackground(dynamic color) {
+    emit(state.copyWith(background: color));
+  }
+
+  void setBoxShadow(bool shadow) {
+    emit(state.copyWith(boxShadow: shadow));
+  }
+
+  void setBlendMode(dynamic blendMode) {
+    emit(state.copyWith(blendMode: blendMode));
+  }
+}
